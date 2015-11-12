99f206a3d3b63c3b58239d09c51f175f856be695
vivek <f2012058@pilani.bits-pilani.ac.in>
2015-11-12 22:13:54 +0530

    Adding subprocess call for opening video file at specified time

---
+++ InVideoSearch.py
@@ -0,0 +1,11 @@
+from subprocess import call
+
+#utility func1 to input keyword from user
+
+#utility func2 to search for keyword in subtitle file
+#returns the list of times where keyword is present
+
+#utility func3 to start video
+
+#starts video at 720 secs
+call(["vlc", "--start-time","720","/home/vivek/Downloads/matrix1999.mp4"])
