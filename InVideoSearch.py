0a6fd0b5d4e2a9b82db392786572d77b47062c58
vivek <f2012058@pilani.bits-pilani.ac.in>
2015-11-12 23:01:20 +0530

    function templates to input key,search and start

--- InVideoSearch.py
+++ InVideoSearch.py
@@ -1,11 +1,41 @@
 from subprocess import call
 
 #utility func1 to input keyword from user
+def inputKeyword():
+
+	return raw_input("Enter key to search in the video: ")
 
 #utility func2 to search for keyword in subtitle file
 #returns the list of times where keyword is present
+def SearchOccurences(key):
+	time=[]
+	with open("/home/vivek/Downloads/matrix1999.srt") as subfile:
+		data=subfile.read()
+		#Preprocess data
+		data=data.lower()	#convert all to lower(Case insensitive search)
+		# convert 're to are, etc.
+		#search for keyword
+		#for word in re.findall('\w+', data):
+		#	print word
+
+
+	return time
 
 #utility func3 to start video
+def startVid(time):
 
+	call(["vlc", "--start-time",time,"/home/vivek/Downloads/matrix1999.mp4"])
+	
 #starts video at 720 secs
-call(["vlc", "--start-time","720","/home/vivek/Downloads/matrix1999.mp4"])
+
+if __name__ == '__main__':
+	keyToSearch=inputKeyword()
+	timesList=SearchOccurences(keyToSearch)
+	for i in timesList:
+		startVid(i)
+		nextK=raw_input()
+		if nextK is "N":
+			continue
+		else:
+			break
+
